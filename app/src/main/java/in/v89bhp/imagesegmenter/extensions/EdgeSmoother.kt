9bc6434b8cd1d721d50afb4f96a97af462fcbb38
VaishakNair <vaishak.k.nair@gmail.com>
2024-02-26 13:42:47 +0530

    Continue...

--- app/src/main/java/in/v89bhp/imagesegmenter/extensions/EdgeSmoother.kt
+++ app/src/main/java/in/v89bhp/imagesegmenter/extensions/EdgeSmoother.kt
@@ -7,49 +7,56 @@ import androidx.core.graphics.get
 import androidx.core.graphics.set
 
 // Size of blurring filter. FILTER_SIZE x FILTER_SIZE filter will be created:
-private const val FILTER_SIZE = 5
+private const val FILTER_SIZE = 9
+// How many pixels starting at the edge pixel should be smoothened horizontally:
+private const val STRIDE = 10
 
 fun Bitmap.smoothenTransparentEdges(): Bitmap {
-    val smoothedBitmap = this.copy(this.config, true)
-    for (rowIndex in 0 until this.height) {
-        for (columnIndex in 0 until this.width) {
-            val maskValue = this[columnIndex, rowIndex]
-            if (maskValue == Color.TRANSPARENT) {
-                try {
-                    if (this[columnIndex + 1, rowIndex] != Color.TRANSPARENT) {
-                        // TODO Edge detected. Do smoothing
+    var smoothenedBitmap = this.copy(this.config, true)
+    var maskedBitmap = this.copy(this.config, true)
 
-                        for (i in 1..20) {
-                            try {
-                                val pixels = Array(FILTER_SIZE * FILTER_SIZE) { 0 }.toIntArray()
-                                this.getPixels(
-                                    pixels,
-                                    0,
-                                    FILTER_SIZE,
-                                    columnIndex + i,
-                                    rowIndex - 1,
-                                    FILTER_SIZE,
-                                    FILTER_SIZE
-                                )
-                                smoothedBitmap[columnIndex + i, rowIndex] =
-                                    getSmoothedPixelValue(pixels)
-                            } catch (e: IllegalArgumentException) {
-                                // Do nothing
+    for (i in 0 until 2) { // Two iterations for smoothening from left to right and from right to left (by rotating the image 180 degrees)
+        for (rowIndex in 0 until maskedBitmap.height) {
+            for (columnIndex in 0 until maskedBitmap.width) {
+                val maskValue = maskedBitmap[columnIndex, rowIndex]
+                if (maskValue == Color.TRANSPARENT) {
+                    try {
+                        if (maskedBitmap[columnIndex + 1, rowIndex] != Color.TRANSPARENT) {
+                            // Edge detected. Do smoothing:
+                            for (i in 1..STRIDE) {
+                                try {
+                                    val pixels = Array(FILTER_SIZE * FILTER_SIZE) { 0 }.toIntArray()
+                                    maskedBitmap.getPixels(
+                                        pixels,
+                                        0,
+                                        FILTER_SIZE,
+                                        columnIndex + i,
+                                        rowIndex - 1,
+                                        FILTER_SIZE,
+                                        FILTER_SIZE
+                                    )
+                                    smoothenedBitmap[columnIndex + i, rowIndex] =
+                                        getSmoothenedPixelValue(pixels)
+                                } catch (e: IllegalArgumentException) {
+                                    // Do nothing
+                                }
                             }
-                        }
 
+                        }
+                    } catch (e: IllegalArgumentException) {
+                        // Do nothing
                     }
-                } catch (e: IllegalArgumentException) {
-                    // Do nothing
                 }
             }
         }
+        smoothenedBitmap = smoothenedBitmap.rotate(180f)
+        maskedBitmap = maskedBitmap.rotate(180f)
     }
 
-    return smoothedBitmap
+    return smoothenedBitmap
 }
 
-fun getSmoothedPixelValue(pixels: IntArray): Int {
+fun getSmoothenedPixelValue(pixels: IntArray): Int {
     val alphas = mutableListOf<Int>()
     val reds = mutableListOf<Int>()
     val greens = mutableListOf<Int>()
